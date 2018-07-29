fe3af0037bd04baf915bcafc72248e785ba1c5b6
TerrellWhite <Terrell.White053100@gmail.com>
2018-07-29 18:42:22 -0400

    First commit

--- GC_Lab_8/Program.cs
+++ GC_Lab_8/Program.cs
@@ -11,15 +11,81 @@ namespace GC_Lab_8
     {
         static void Main(string[] args)
         {
-            string [] names = { "Terrell", "Samantha", "Kristen", "Chris", "Ash", "Drew", "Terri", "Jacob", "Tesla", "Boi", "Austin Powers", "Chief QueeT", "Big Dog Bark", "21 Average", "What's your name again", "Zack", "Jesse","Ray","Bodum","Patrick" };
-            string[] hometown = { "Detroit MI", "Ann-Dan MI", "Plaztic Beach", "New Port Rhode Island ", "Pallet Town", "Break Yo Code Island", "Detroit MI", "Somewhere in Africa", "Somewhere in Africa", "Terrell's Mind", "Great Britain", "Sooo Close", "Dog Island", "Sound Cloud", "Who cares", "Detroit MI", "Royal Oak. MI", "Charlesville", "Rock Heaven", "Somewhere" };
-            string[] favFood = { "Hard Drugs", "Pizza", "Rice I think", "Tacos", "Tacos", "Happiness", "Coffee", "Powder", "Chicken", "Cheese Feet", "Hair", "inappropriate", "Dog Food", "Not Lyrics", "Who cares", "Face Cream", "Books", "Tacos", "Pasta", "Something" };
+            List<string> names = new List<string>();
+            names.Add("Terrell");
+            names.Add("Samantha");
+            names.Add("Kristen");
+            names.Add("Chris");
+            names.Add("Ash");
+            names.Add("Drew");
+            names.Add("Terri");
+            names.Add("Jacob");
+            names.Add("Tesla");
+            names.Add("Boi");
+            names.Add("Austin Powers");
+            names.Add("Chief QueeT");
+            names.Add("Big Dog Bark");
+            names.Add("21 Average");
+            names.Add("What's your name again");
+            names.Add("Zack");
+            names.Add("Jesse");
+            names.Add("Ray");
+            names.Add("Bodum");
+            names.Add("Patrick");
+            List<string> homeTown = new List<string>();
+            homeTown.Add("Detroit MI");
+            homeTown.Add("Ann-Dan MI");
+            homeTown.Add("Plaztic Beach");
+            homeTown.Add("New Port Rhode Island ");
+            homeTown.Add("Pallet Town");
+            homeTown.Add("Break Yo Code Island");
+            homeTown.Add("Detroit MI");
+            homeTown.Add("Somewhere in Africa");
+            homeTown.Add("Somewhere in Africa");
+            homeTown.Add("Terrell's Mind");
+            homeTown.Add("Great Britain");
+            homeTown.Add("Sooo Close");
+            homeTown.Add("Dog Island");
+            homeTown.Add("Sound Cloud");
+            homeTown.Add("Who cares");
+            homeTown.Add("Detroit MI");
+            homeTown.Add("Royal Oak. MI");
+            homeTown.Add("Charlesville");
+            homeTown.Add("Rock Heaven");
+            homeTown.Add("Somewhere");
+            List<string> favFood = new List<string>();
+            favFood.Add("Hard Drugs");
+            favFood.Add("Pizza");
+            favFood.Add("Rice I think");
+            favFood.Add("Tacos");
+            favFood.Add("Tacos");
+            favFood.Add("Happiness");
+            favFood.Add("Coffee");
+            favFood.Add("Powder");
+            favFood.Add("Chicken");
+            favFood.Add("Cheese Feet");
+            favFood.Add("Hair");
+            favFood.Add("inappropriate");
+            favFood.Add("Dog Food");
+            favFood.Add("Not Lyrics");
+            favFood.Add("Who cares");
+            favFood.Add("Face Cream");
+            favFood.Add("Books");
+            favFood.Add("Tacos");
+            favFood.Add("Pasta");
+            favFood.Add("Something");
             int input = GetVaildNumInput(@"^\d{1,2}", "Pick a number and learn about a person:");
             string ans = GetVaildInput(@"^[yY]|[nN]{1}$", $"You picked {names[input]}, would you like to learn more about this person(y/n)");
             if (ans.ToLower() == "y")
             {
-                Console.WriteLine($"{names[input]} is from {hometown[input]} and his or here favorite food is {favFood[input]} ");
+                Console.WriteLine($"{names[input]} is from {homeTown[input]} and his or here favorite food is {favFood[input]} ");
             }
+            ans = GetVaildInput(@"^[yY]|[nN]{1}$", "Would you like to add any more information about this student(y/n)");
+            if (ans.ToLower() == "y")
+            {
+                AddFacts(@"^.{1,20}$", "Would you like to input information about the Favorite Color or about Their Favorite Number");
+            }
+
         }
         public static int GetVaildNumInput(string pattern, string userMessage, string errorMessage = "Wrong Input Try Again")
         {
@@ -35,7 +101,7 @@ namespace GC_Lab_8
             return int.Parse(input);
 
         }
-                public static string GetVaildInput(string pattern, string userMessage, string errorMessage = "Wrong Input Try Again")
+       public static string GetVaildInput(string pattern, string userMessage, string errorMessage = "Wrong Input Try Again")
         {
             Console.WriteLine(userMessage);
             string input = Console.ReadLine();
@@ -47,5 +113,46 @@ namespace GC_Lab_8
             }
             return input;
         }
+        public static void AddFacts(string pattern, string userMessage, string errorMessage = "Wrong Input Try Again")
+        {
+            Console.WriteLine(userMessage);
+            string input = Console.ReadLine();
+            while (!(Regex.IsMatch(input, pattern)))
+            {
+                Console.WriteLine(errorMessage);
+                input = Console.ReadLine();
+
+            }
+            int sum = 0;
+            if (input.ToLower() == "favorite color" || input.ToLower() == "color")
+            {
+                List<string> favorColor = new List<string>();
+                Console.WriteLine("Enter their favorite color:");
+                favorColor.Add(Console.ReadLine());
+                Console.WriteLine($"Their favorite color is {favorColor[0]}");
+                sum += 1;
+                string ans = GetVaildInput(@"^[yY]|[nN]{1}$", "Would you like to add any more information about this student's favorite number(y/n)");
+                if (ans.ToLower() == "y")
+                {
+                    int inputnum = GetVaildNumInput(@"^\d{0,20}$", "What is their favorite number?");
+                    Console.WriteLine($"Their favorite number is {inputnum}");
+                }
+            }
+            else if (input.ToLower() == "favorite number" || input.ToLower() == "number")
+            {
+                List<string> favorNumber = new List<string>();
+                Console.WriteLine("Enter their favorite number:");
+                favorNumber.Add(Console.ReadLine());
+                Console.WriteLine($"Their favorite number is {favorNumber[0]}");
+                sum += 1;
+                string ans = GetVaildInput(@"^[yY]|[nN]{1}$", "Would you like to add any more information about this student's favorite color(y/n)");
+                if (ans.ToLower() == "y")
+                {
+                    int inputnum = GetVaildNumInput(@"^\d{1,20}$", "What is their favorite number?");
+                    Console.WriteLine($"Their favorite number is {inputnum}");
+                }
+            }
+        }
+        
     }
 }
